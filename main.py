b78ff3ecd9c728dec729debecfab1687a22115f1
Krzaq8 <kubakropkazet@gmail.com>
2022-05-13 23:31:50 +0200

    po dodaniu auth działa na studentsie

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-from get_data import get_substance_table, get_result_table
-import csv
+from get_data import get_substance_name, get_substance_table, get_result_table
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "2b3f12f3ef12a6c86b"
 Bootstrap(app)
 
 @app.route("/")
@@ -16,14 +16,11 @@ def home():
 @app.route("/results")
 def results():
     # TODO tutaj wstawiam funkcję od Asi, która zwraca tabelę wynikową dla konkretnej substancji aktywnej
-    result_table = get_result_table(request.args.get('sub'))
+    sub_id = request.args.get('sub')
+    result_table = get_result_table(sub_id)
+    sub_name = get_substance_name(sub_id)
     # result_table = [["h1", "h2", "h3", "h4"], ["a", "b", "c", "d"], ["e", "f", "g", "h"], ["i", "j", "k", "l"]]
-    return render_template("results.html", table=result_table)
-
-def get_substance_list(file_address):
-    with open(file_address) as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        return next(reader)
+    return render_template("results.html", table=result_table, sub_name=sub_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
