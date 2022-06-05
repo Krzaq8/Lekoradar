37bb29535a37da549f03c324f639749f5b72445f
Tomek <t.pieszczek@student.uw.edu.pl>
2022-06-05 17:28:48 +0200

    scalanie komórek, guzik powrotu, tytuł karty

--- main.py
+++ main.py
@@ -16,12 +16,32 @@ def results():
     sub_id = request.args.get('sub')
     result_table = get_result_table(sub_id)
     sub_name = get_substance_name(sub_id)
-    return render_template("results.html", table=result_table, sub_name=sub_name)
+    res_dict = get_cell_merge_dict(result_table)
+    return render_template("results.html", table=result_table, 
+        sub_name=sub_name, res_dict=res_dict)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template('error.html') 
+
+
+def get_cell_merge_dict(result_table):
+    res_dict = {}
+    last_group_num = 1
+    last_index = 1
+    rowspan_counter = 0
+    
+    for i in range(1, len(result_table)):
+        if last_group_num != result_table[i][0]:
+            res_dict[last_index + 1] = rowspan_counter
+            last_group_num = result_table[i][0]
+            rowspan_counter = 1
+            last_index = i
+        else:
+            rowspan_counter += 1
+    res_dict[last_index + 1] = rowspan_counter
+    return res_dict
 
 if __name__ == "__main__":
     app.run(debug=True)
